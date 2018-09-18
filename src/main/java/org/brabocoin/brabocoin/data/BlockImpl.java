1fbf4f2f13311d85430a0f889489d524fa472450
David Dekker <d.j.c.dekker@student.tue.nl>
2018-09-18 15:04:31 +0200

    Completed block implementation

--- src/main/java/org/brabocoin/brabocoin/data/BlockImpl.java
+++ src/main/java/org/brabocoin/brabocoin/data/BlockImpl.java
@@ -2,16 +2,39 @@ package org.brabocoin.brabocoin.data;
 
 public class BlockImpl implements Block {
 
-    private Hash previousBlockHash;
+    /**
+     * Hash of previous block.
+     */
+    private final Hash previousBlockHash;
 
-    private Hash merkleRoot;
+    /**
+     * Hash of the merkle root of this block.
+     */
+    private final Hash merkleRoot;
 
-    private Hash targetValue;
+    /**
+     * Target value of the proof-of-work of this block.
+     */
+    private final Hash targetValue;
 
-    private long nonce;
+    /**
+     * Nonce used for the proof-of-work.
+     */
+    private final long nonce;
 
-    private long timestamp;
+    /**
+     * Timestamp when the block is created.
+     */
+    private final long timestamp;
 
+    /**
+     * Create a new block.
+     * @param previousBlockHash hash of previous block
+     * @param merkleRoot hash of merkle root
+     * @param targetValue target value of proof-of-work
+     * @param nonce nonce for proof-of-work
+     * @param timestamp timestamp when block is created
+     */
     public BlockImpl(Hash previousBlockHash, Hash merkleRoot, Hash targetValue,
             long nonce, long timestamp) {
         this.previousBlockHash = previousBlockHash;
