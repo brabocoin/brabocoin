d89e7cd057d8a9db2a0126c691d4d38a5efffb84
Sten Wessel <sten.wessel@gmail.com>
2018-10-22 15:50:39 +0200

    Extracted NewBlockStatus to top level

--- src/main/java/org/brabocoin/brabocoin/chain/BlockProcessor.java
+++ src/main/java/org/brabocoin/brabocoin/chain/BlockProcessor.java
@@ -302,30 +302,4 @@ public class BlockProcessor {
         blockchain.getMainChain().pushTipBlock(tip);
     }
 
-    /**
-     * The result of processing a new block.
-     */
-    public enum NewBlockStatus {
-
-        /**
-         * The block is added as orphan because not all ancestors of the block are already known.
-         */
-        ORPHAN,
-
-        /**
-         * The new block is successfully added to the blockchain, either on the main chain or as
-         * a fork.
-         */
-        ADDED_TO_BLOCKCHAIN,
-
-        /**
-         * The block has already been processed and stored.
-         */
-        ALREADY_STORED,
-
-        /**
-         * The block is invalid and has not been stored.
-         */
-        INVALID
-    }
 }
---
+++ src/main/java/org/brabocoin/brabocoin/chain/NewBlockStatus.java
@@ -0,0 +1,28 @@
+package org.brabocoin.brabocoin.chain;
+
+/**
+ * The result of processing a new block.
+ */
+public enum NewBlockStatus {
+
+    /**
+     * The block is added as orphan because not all ancestors of the block are already known.
+     */
+    ORPHAN,
+
+    /**
+     * The new block is successfully added to the blockchain, either on the main chain or as
+     * a fork.
+     */
+    ADDED_TO_BLOCKCHAIN,
+
+    /**
+     * The block has already been processed and stored.
+     */
+    ALREADY_STORED,
+
+    /**
+     * The block is invalid and has not been stored.
+     */
+    INVALID
+}
