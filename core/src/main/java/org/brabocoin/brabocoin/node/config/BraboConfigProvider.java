1618f43c924a7c0a947eedcbc9934a72fbc1c356
Sten Wessel <s.wessel@student.tue.nl>
2018-11-18 15:16:27 +0100

    BRABO-94 Extracted core module and include config in classpath such that it can be loaded from a JAR file

--- core/src/main/java/org/brabocoin/brabocoin/node/config/BraboConfigProvider.java
+++ core/src/main/java/org/brabocoin/brabocoin/node/config/BraboConfigProvider.java
@@ -3,10 +3,8 @@ package org.brabocoin.brabocoin.node.config;
 import org.cfg4j.provider.ConfigurationProvider;
 import org.cfg4j.provider.ConfigurationProviderBuilder;
 import org.cfg4j.source.ConfigurationSource;
-import org.cfg4j.source.context.environment.Environment;
-import org.cfg4j.source.context.environment.ImmutableEnvironment;
+import org.cfg4j.source.classpath.ClasspathConfigurationSource;
 import org.cfg4j.source.context.filesprovider.ConfigFilesProvider;
-import org.cfg4j.source.files.FilesConfigurationSource;
 
 import java.nio.file.Paths;
 import java.util.Collections;
@@ -16,16 +14,13 @@ import java.util.Collections;
  */
 public class BraboConfigProvider {
     private final static String configFile = "application.yaml";
-    private final static String configDirectory = "./config";
 
     public static ConfigurationProvider getConfig() {
         final ConfigFilesProvider configFilesProvider = () -> Collections.singletonList(Paths.get(configFile));
-        final ConfigurationSource source = new FilesConfigurationSource(configFilesProvider);
-        final Environment environment = new ImmutableEnvironment(configDirectory);
+        final ConfigurationSource source = new ClasspathConfigurationSource(configFilesProvider);
 
         return new ConfigurationProviderBuilder()
                 .withConfigurationSource(source)
-                .withEnvironment(environment)
                 .build();
     }
 }
