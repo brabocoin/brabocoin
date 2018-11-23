e255605bb3ff3af100c79dec1917dce250159e11
Dennis van den Berg <dennispvandenberg@gmail.com>
2018-11-23 16:26:16 +0100

    BRABO-83 WIP

--- core/src/main/java/org/brabocoin/brabocoin/wallet/Wallet.java
+++ core/src/main/java/org/brabocoin/brabocoin/wallet/Wallet.java
@@ -2,7 +2,13 @@ package org.brabocoin.brabocoin.wallet;
 
 import org.brabocoin.brabocoin.crypto.PublicKey;
 import org.brabocoin.brabocoin.dal.ReadonlyUTXOSet;
+import org.brabocoin.brabocoin.exceptions.DatabaseException;
+import org.brabocoin.brabocoin.model.Input;
+import org.brabocoin.brabocoin.model.Transaction;
+import org.brabocoin.brabocoin.model.UnsignedTransaction;
 import org.brabocoin.brabocoin.model.crypto.PrivateKey;
+import org.brabocoin.brabocoin.model.dal.UnspentOutputInfo;
+import org.brabocoin.brabocoin.util.Destructible;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
@@ -48,6 +54,60 @@ public class Wallet implements Iterable<Map.Entry<PublicKey, PrivateKey>> {
      */
     public Collection<PrivateKey> getPrivateKeys() {
         return keyMap.values();
+    }
+
+
+
+    /**
+     * Whether the private key corresponding to the given public key is encrypted.
+     *
+     * @param publicKey The public key used to search the private key
+     * @return Whether the matching private key is encrypted
+     */
+    public boolean isKeyPairEncrypted(PublicKey publicKey) {
+        if (!keyMap.containsKey(publicKey)) {
+            throw new IllegalStateException("Public key does not exists in keymap");
+        }
+
+        return keyMap.get(publicKey).isEncrypted();
+    }
+
+    /**
+     * Sign an unsigned transaction to create a signed transaction.
+     *
+     * @param transaction Unsigned transaction
+     * @return A signed transaction
+     */
+    public Transaction signTransaction(UnsignedTransaction transaction,
+                                       Map<PublicKey, Destructible<char[]>> passphrases) throws DatabaseException {
+        for (Input input : transaction.getInputs()) {
+            UnspentOutputInfo outputInfo = utxoSet.findUnspentOutputInfo(input);
+            if (outputInfo == null) {
+                throw new IllegalStateException("Could not find output info for input in the transaction");
+            }
+
+            PublicKey publicKey = getPublicKeys().stream()
+                    .filter(pub -> pub.getHash().equals(outputInfo.getAddress()))
+                    .findFirst()
+                    .orElse(null);
+
+            if (publicKey == null) {
+                throw new IllegalStateException("Referenced output uses address not found in wallet.");
+            }
+
+            PrivateKey privateKey = keyMap.get(publicKey);
+
+            if (privateKey == null) {
+                throw new IllegalStateException("Public key has null private key in keymap.");
+            }
+
+            if (!privateKey.isUnlocked()) {
+                // TODO: Implement
+            }
+        }
+
+        // TODO: WIP here..
+        return null;
     }
 
     @NotNull
