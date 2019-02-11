576fd4ecf637654ecd42c99c8c2b671027244ac8
Sten Wessel <s.wessel@student.tue.nl>
2019-02-11 12:27:35 +0100

    BRABO-237 Custom type parsing for target value in config

--- core/src/main/java/org/brabocoin/brabocoin/node/config/CustomTypeConfigProvider.java
+++ core/src/main/java/org/brabocoin/brabocoin/node/config/CustomTypeConfigProvider.java
@@ -1,10 +1,19 @@
 package org.brabocoin.brabocoin.node.config;
 
+import com.github.drapostolos.typeparser.NoSuchRegisteredParserException;
+import com.github.drapostolos.typeparser.Parser;
 import com.github.drapostolos.typeparser.TypeParser;
+import com.github.drapostolos.typeparser.TypeParserBuilder;
+import com.github.drapostolos.typeparser.TypeParserException;
 import org.cfg4j.provider.ConfigurationProvider;
 import org.cfg4j.provider.GenericTypeInterface;
+import org.cfg4j.source.ConfigurationSource;
+import org.cfg4j.source.context.environment.Environment;
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.NoSuchElementException;
 import java.util.Properties;
 
 /**
@@ -12,10 +21,22 @@ import java.util.Properties;
  */
 public class CustomTypeConfigProvider implements ConfigurationProvider {
 
+    private final @NotNull TypeParserBuilder parserBuilder;
+    private final @NotNull ConfigurationSource configurationSource;
+    private final @NotNull Environment environment;
     private final @NotNull ConfigurationProvider provider;
 
-    public CustomTypeConfigProvider(@NotNull ConfigurationProvider provider) {
+    public CustomTypeConfigProvider(@NotNull ConfigurationSource configurationSource,
+                                    @NotNull Environment environment,
+                                    @NotNull ConfigurationProvider provider) {
+        this.parserBuilder = TypeParser.newBuilder();
+        this.configurationSource = configurationSource;
+        this.environment = environment;
         this.provider = provider;
+    }
+
+    public <T> void addParser(Class<T> targetType, Parser<T> parser) {
+        parserBuilder.registerParser(targetType, parser);
     }
 
     @Override
@@ -25,17 +46,58 @@ public class CustomTypeConfigProvider implements ConfigurationProvider {
 
     @Override
     public <T> T getProperty(String key, Class<T> type) {
-        TypeParser parser = TypeParser.newBuilder().
-        return null;
+        String propertyStr = getProperty(key);
+        TypeParser parser = parserBuilder.build();
+
+        try {
+            return parser.parse(propertyStr, type);
+        }
+        catch (TypeParserException | NoSuchRegisteredParserException e) {
+            throw new IllegalArgumentException("Unable to cast value \'" + propertyStr + "\' to " + type, e);
+        }
     }
 
     @Override
     public <T> T getProperty(String key, GenericTypeInterface genericType) {
-        return null;
+        String propertyStr = getProperty(key);
+        TypeParser parser = parserBuilder.build();
+
+        try {
+            //noinspection unchecked
+            return (T)parser.parseType(propertyStr, genericType.getType());
+        }
+        catch (TypeParserException | NoSuchRegisteredParserException e) {
+            throw new IllegalArgumentException("Unable to cast value \'" + propertyStr + "\' to " + genericType, e);
+        }
     }
 
     @Override
     public <T> T bind(String prefix, Class<T> type) {
-        return provider.bind();
+        // Call actual implementation using reflection
+        try {
+            Method method = provider.getClass().getDeclaredMethod("bind", ConfigurationProvider.class, String.class, Class.class);
+            method.setAccessible(true);
+            //noinspection unchecked
+            return (T)method.invoke(provider, this, prefix, type);
+        }
+        catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new IllegalStateException("Could not find binding implementation");
+        }
+    }
+
+    private String getProperty(String key) {
+        try {
+
+            Object property = configurationSource.getConfiguration(environment).get(key);
+
+            if (property == null) {
+                throw new NoSuchElementException("No configuration with key: " + key);
+            }
+
+            return property.toString();
+
+        } catch (IllegalStateException e) {
+            throw new IllegalStateException("Couldn't fetch configuration from configuration source for key: " + key, e);
+        }
     }
 }
