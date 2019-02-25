baae46bbd8b131d618c3db5403cfae528266306b
Sten Wessel <s.wessel@student.tue.nl>
2019-02-25 15:46:37 +0100

    Funny table resize animation

--- gui/src/main/java/org/brabocoin/brabocoin/gui/control/AutoResizeTableSkin.java
+++ gui/src/main/java/org/brabocoin/brabocoin/gui/control/AutoResizeTableSkin.java
@@ -1,6 +1,8 @@
 package org.brabocoin.brabocoin.gui.control;
 
 import com.sun.javafx.scene.control.skin.TableViewSkin;
+import com.sun.javafx.scene.control.skin.VirtualFlow;
+import javafx.scene.control.IndexedCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
@@ -19,9 +21,14 @@ public class AutoResizeTableSkin<T> extends TableViewSkin<T> {
     @Override
     protected void updateRowCount() {
         super.updateRowCount();
+
+        VirtualFlow<?> virtualFlow = (VirtualFlow<?>)getChildren().get(1);
+        IndexedCell lastVisibleCell = virtualFlow.getLastVisibleCell();
+        if (lastVisibleCell == null) return;
+
         for (TableColumn<T, ?> col : tableView.getColumns()) {
             if (this.getTableHeaderRow().getColumnHeaderFor(col) != null) {
-                this.resizeColumnToFitContent(col, 1);
+                this.resizeColumnToFitContent(col, lastVisibleCell.getIndex());
             }
         }
     }
